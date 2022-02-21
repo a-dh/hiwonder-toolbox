7ab2d457f9cb53bed4ecd3878874498dea117fd8
Albert den Haan <albert.denhaan@gmail.com>
2022-02-21 06:16:32 +0000

    Update README.md for hw_button_scan
    Also add logging to hw_button_scan

--- README.md
+++ README.md
@@ -5,6 +5,14 @@
     python3 -m venv venv
     . venv/bin/activate
     pip install -r requirements.txt
+    sudo make install
+
+After the ```make install``` succeeds there will be a service process
+monitoring button 2 on the HAT (The left-most on the edge with the
+power switch)  Pressing it for more than 3(?) seconds will start the
+shutdown process.  Look for the green activity light on the RPI4 board
+(farthest from the ethernet RJ-45 jack) to stop flickering and the power
+can be safely turned off
 
 
 # Ubuntu release
--- hw_button_scan.py
+++ hw_button_scan.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+import logging
+import logging.handlers
 import os, sys, time
 import RPi.GPIO as GPIO
 
@@ -9,6 +13,14 @@ def reset_wifi():
     os.system("systemctl restart hw_wifi.service > /dev/null 2>&1")
 
 if __name__ == "__main__":
+
+    my_logger = logging.getLogger('hw_button_scan')
+    my_logger.setLevel(logging.DEBUG)
+
+    handler = logging.handlers.SysLogHandler(address = '/dev/log')
+
+    my_logger.addHandler(handler)
+
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(key1_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.setup(key2_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -25,10 +37,10 @@ if __name__ == "__main__":
                     count = 0
                 else:
                     count += 1
-                    if count == 40:
+                    if count >= 40:
                         count = 0
                         key1_pressed = False
-                        print('restart wifi')
+                        my_logger.info('Key 1 pressed for a while')
                         reset_wifi()
             else:
                 count = 0
@@ -42,10 +54,10 @@ if __name__ == "__main__":
                     count = 0
                 else:
                     count += 1
-                    if count == 40:
+                    if count >= 40:
                         count = 0
                         key2_pressed = False
-                        print('shutdown')
+                        my_logger.info('Key 2 pressed for a while: shutdown')
                         os.system('sync')
                         os.system('sudo poweroff -f -p')
             else:
